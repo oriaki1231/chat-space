8088cc5529885a71a38b11de5b04a54e180061ba
oriaki1231 <aki.raliver.1231@icloud.com>
2020-02-10 15:12:16 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :groups
-  has_many :groups through:  :group_users
+  has_many :groups, through:  :group_users
   validates :name, presence: true
 end
